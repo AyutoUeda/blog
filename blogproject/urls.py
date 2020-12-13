d9c5f2c05c48efdc7d1ea836fe3d79a92744f942
AyutoUeda <ayuto.ueda@gmail.com>
2020-12-13 10:27:54 +0900

    master

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, incluse
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', incluse('blogpost.urls'))
+    path('', include('blogpost.urls'))
 ]
