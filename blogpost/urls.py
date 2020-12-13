41d71b947d3b8125b8b546f9cba6fb8d111af361
AyutoUeda <ayuto.ueda@gmail.com>
2020-12-14 01:00:58 +0900

    master

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import BlogList
+from .views import BlogList, BlogDetail
 
 urlpatterns = [
     path('list/', BlogList.as_view()),
+    path('detail/<int:pk>/', BlogDetail.as_view())
 ]
+
+# <int:pk>/ はテーブルに入っているデータを具体的に指定する上で使われるコード
+# intはintegerの略であり、整数型のあることを明示する
+# pkはprimary keyの略。データベースがデータを特定するときに使われる項目のことを意味する。
+# blogpost/migrations/0002_blogmodel.py を参照
--- blogpost/views.py
+++ blogpost/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
+from django.views.generic.detail import DetailView
 from .models import BlogModel
 
 # Create your views here.
 class BlogList(ListView):
   template_name = 'list.html'
+  model = BlogModel
+
+class BlogDetail(DetailView):
+  template_name = 'detail.html'
   model = BlogModel
