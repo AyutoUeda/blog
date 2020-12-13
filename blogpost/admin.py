4965611b195a6b9117f30b8bfdbd8779b431f893
AyutoUeda <ayuto.ueda@gmail.com>
2020-12-13 18:38:50 +0900

    naster

--- blogpost/admin.py
+++ blogpost/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import register
-from .models import SampleModel
+from .models import SampleModel, BlogModel
 
 # Register your models here.
 
 admin.site.register(SampleModel)
+admin.site.register(BlogModel)
